516020e68a965ffe788eb26893a2f4652af45822
vaquero <froilan@virtual-artifact.com>
2024-04-11 19:10:07 +0200

    feature: add all initial files

--- app/server.py
+++ app/server.py
@@ -1,27 +1,26 @@
-from starlette.applications import Response
-from starlette.responses import JSONResponse
-from starlette.routing import Route
 import json
 import aiohttp
-
 import asyncio
 import uvicorn
 from fastai import *
 from fastai.text.all import *
-# from fastai.vision import *
 from io import BytesIO
 from starlette.applications import Starlette
+from starlette.applications import Response
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
+from starlette.routing import Route
 
-export_file_url = 'https://www.dropbox.com/scl/fi/e8oes6vu6glw1c1iusy2u/imdb_export.pkl?rlkey=a1sngtxywaqikzg5py1yrxd6o&dl=1'
-export_file_name = 'imdb_export.pkl'
+export_file_url_imdb = 'https://www.dropbox.com/scl/fi/e8oes6vu6glw1c1iusy2u/imdb_export.pkl?rlkey=a1sngtxywaqikzg5py1yrxd6o&dl=1'
+export_file_url_toxic = 'https://www.dropbox.com/scl/fi/t68cwt21h0jnn7y28pi9q/toxic_comment_classifier.pkl?rlkey=7sn1f9dakmnf8myxhyqidrdv3&dl=1'
+export_file_name_imdb = 'imdb_export.pkl'
+export_file_name_toxic = 'toxic_comment_classifier.pkl'
 
 path = Path(__file__).parent
+# path = os.path.join(os.path.dirname(__file__), './models/')
 
 app = Starlette()
-# app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
 origins = ["*"]
 
 app.add_middleware(
@@ -42,9 +41,9 @@ async def download_file(url, dest):
 
 
 async def setup_learner():
-    await download_file(export_file_url, path / export_file_name)
+    await download_file(export_file_url_toxic, path / export_file_name_toxic)
     try:
-        learn = load_learner(path / export_file_name)
+        learn = load_learner(path / export_file_name_toxic)
         return learn
     except RuntimeError as e:
         if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
@@ -63,17 +62,11 @@ loop.close()
 
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
-    # img_data = await request.form()
-    # img_bytes = await (img_data['file'].read())
-    # img = open_image(BytesIO(img_bytes))
-    # prediction = learn.predict(img)[0]
-    # return JSONResponse({'result': str(prediction)})
     body = await request.json()
     prediction = learn.predict(body['text'])
     print(prediction)
-    content = json.dumps(prediction[0])
-    # response = Response(content,status_code=200, headers=None, media_type='application/json')
-    response = JSONResponse({'result': str(prediction)})
+    #content = json.dumps(prediction[0])
+    response = JSONResponse({'result': str(prediction[0])})
     return response
 
 
